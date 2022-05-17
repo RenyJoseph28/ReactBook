5adeb263f8397333984fa4ca717ea5feb3bae750
RenyJoseph28 <ajaydvrj@gmail.com>
2022-05-17 13:18:12 +0530

    navebar complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import  'bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addbooks from './component/Addbooks';
 import Searchbook from './component/Searchbook';
+import Navebar from './component/Navebar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Addbooks/>
-      <Searchbook/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={ <Addbooks/>}/>
+        <Route path='/search' exact element={ <Searchbook/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
